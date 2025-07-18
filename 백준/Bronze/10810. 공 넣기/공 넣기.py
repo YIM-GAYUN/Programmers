6a3b28cd883e21c50b24beb095dcf4a8733af69b
YIM-GAYUN <160388960+YIM-GAYUN@users.noreply.github.com>
2025-07-18 22:49:41 +0900

    [Bronze III] Title: 공 넣기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+arr = [0]*n
+for i in range(m):
+    a, b, c = map(int, input().split())
+    for j in range(a-1, b):
+        arr[j] = c
+for k in range(n):
+    print(arr[k], end=' ')

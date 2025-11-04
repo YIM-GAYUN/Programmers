112b1707646ad94d7d2a75bbdb4d40aec1e877e5
YIM-GAYUN <160388960+YIM-GAYUN@users.noreply.github.com>
2025-11-04 12:28:44 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,9 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    h = 0
+    for i, c in enumerate(citations, start=1):
+        if c >= i:
+            h = i
+        else:
+            break
+    return h

1f987ac9e98990d5c8f0363b703175be33bd739a
YIM-GAYUN <160388960+YIM-GAYUN@users.noreply.github.com>
2025-11-04 10:26:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.11 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,22 +1,15 @@
-'''
-1. X    2. 동명이인 - 번호 부여    3. 배열    4. 한 명 제외    5. 해시
-'''
 def solution(participant, completion):
-    dic = {} # 해시 테이블
-    
-    # 참가자 추가
+    parti_ = {}
     for p in participant:
-        if p in dic:
-            dic[p] += 1
+        if p in parti_:
+            parti_[p] += 1
         else:
-            dic[p] = 1
-    
-    # 완주자 키 값 1씩 감소
+            parti_[p] = 1
+
     for c in completion:
-        dic[c] -= 1
-    
-    # 남이 있는 선수 반환
-    for key in dic.keys():
-        if dic[key] > 0:
+        parti_[c] -= 1
+            
+    for key in parti_.keys():
+        if parti_[key] > 0:
             return key
-    return 0
+    

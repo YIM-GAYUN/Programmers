2ed1a211ddc83555f0bc79ce84a4a2e1d891fc69
YIM-GAYUN <160388960+YIM-GAYUN@users.noreply.github.com>
2025-11-04 12:00:52 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,7 @@
 def solution(array, commands):
     answer = []
-    for i in range(len(commands)):
-        start = commands[i][0] - 1
-        end = commands[i][1]
-        num = commands[i][2]
-        
-        new = array[start:end]
-        set_new = sorted(new)
-        ans = set_new[num - 1]
-        answer.append(ans)
+    for com in commands:
+        temp = array[com[0]-1:com[1]]
+        temp.sort()
+        answer.append(temp[com[2]-1])
     return answer
